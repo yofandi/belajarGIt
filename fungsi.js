97603a6c45b799d7a43826ae622ed82dbe68f448
yofandi riki winata <yofandirikiwinata24@gmail.com>
2024-03-19 09:27:36 +0700

    mencoba menambahkan dua file dan salah satu baru ditambah ketika hendak diadd

--- fungsi.js
+++ fungsi.js
@@ -2,3 +2,9 @@ console.log('hallo, ini yang pertama');
 
 // git init buat repository
 // git add fungsi.js tambah file
+// git commit -m "menambahkan file commit"
+
+const add = (a, b) => a + b;
+console.log(add(1,2));
+
+// git diff untuk melihat segala perubahan yang terjadi
