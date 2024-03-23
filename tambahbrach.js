94626382e36ec4efa828fa30e60e9c5c13868658
yofandi riki winata <yofandirikiwinata24@gmail.com>
2024-03-23 12:41:18 +0700

    menambahka perkalian

--- tambahbrach.js
+++ tambahbrach.js
@@ -1,1 +1,4 @@
-console.log('ini untuk mencoba menambah branch dan merge')
+console.log('ini untuk mencoba menambah branch dan merge');
+
+const multiply = (a, b) => a * b;
+console.log(multiply(4,4));
