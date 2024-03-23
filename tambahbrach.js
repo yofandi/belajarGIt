11cabcbc2a85a54c30f2fd67520fbcd92ac7c52e
yofandi riki winata <yofandirikiwinata24@gmail.com>
2024-03-23 12:47:20 +0700

    menambahka pembagian

--- tambahbrach.js
+++ tambahbrach.js
@@ -2,3 +2,6 @@ console.log('ini untuk mencoba menambah branch dan merge');
 
 const multiply = (a, b) => a * b;
 console.log(multiply(4,4));
+
+const divide = (a, b) => a / b;
+console.log(divide(4,4));
