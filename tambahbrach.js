f990e8dd9e3d8ec9cc57075a675c170eb3af9405
yofandi riki winata <yofandirikiwinata24@gmail.com>
2024-03-23 13:05:30 +0700

    merevisi git perkalian

--- tambahbrach.js
+++ tambahbrach.js
@@ -1,10 +1,10 @@
 console.log('ini untuk mencoba menambah branch dan merge');
 
-const multiply = (a, b) => a * b;
-console.log(multiply(4,4));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(4,4));
 
-const divide = (a, b) => a / b;
-console.log(divide(4,4));
+const divideNumber = (a, b) => a / b;
+console.log(divideNumber(4,4));
 
 // revisi commit
 // -- checkout : sifatnya untuk sementara, melihat perubahan apa saja yang terjadi dalam code 
