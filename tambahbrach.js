7075188c17c41f0d23d2f785dddf1531de13a5b8
yofandi riki winata <yofandirikiwinata24@gmail.com>
2024-03-23 13:02:44 +0700

    menambahkan penjelasan terkait revisi revert

--- tambahbrach.js
+++ tambahbrach.js
@@ -5,3 +5,8 @@ console.log(multiply(4,4));
 
 const divide = (a, b) => a / b;
 console.log(divide(4,4));
+
+// revisi commit
+// -- checkout : sifatnya untuk sementara, melihat perubahan apa saja yang terjadi dalam code 
+// -- reset  :  akan mengembalikan code ke posisi commit yang hendak dikembalikan dengan menghapus beberapa commit setelah commint yang dituju
+// -- revert :  mengembalikan code ke posisi commit yang hendak dikembalikan tanpa menghapus beberapa commit setelah commint yang dituju (rekom)
